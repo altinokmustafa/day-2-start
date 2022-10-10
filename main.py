fd028fdc42357330b6b9e14bf88c921adca06b4e
Mustafa Altinok <64875678+altinokmustafa@users.noreply.github.com>
2022-10-10 19:13:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+#Data Types
+
+
+#Strings
+
+#Subscripting
+
+#print("Hello"[0])
+#H
+#print("Hello"[4])
+#o
+
+#print("123" + "345")
+#123345
+
+#print( "hello" + " world" )
+#hello world
+
+#Integer
+
+#print(123 + 345)
+#468
+  #Large Integers
+#print(342_654_896 + 342_654_896)
+#685309792
+
+
+#Float
+#3.14159
+
+
+#Boolean
+#True
+#False
+
+
+#num_char = len(input("What is your name?"))#
+#print("Your name has" + num_char + "characters.")
+
+#new_num_char = str(num_char)#
+#print("Your name has " + new_num_char + " characters.")#
+#What is your name?Angela
+#Your name has 6 characters.
+
+
+#a = float(123)
+#print(type(a))
+#<class 'float'>
+
+#a = str(123)
+#print(type(a))
+#<class 'str'>
+
+a = str(123)
+print(type(a))
+
+#print(70 + float("100.5"))
+#170.5
+
+
+#print(str(70) + str(100))
+#70100
+
